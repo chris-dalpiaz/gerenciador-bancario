767e52bc4ec38efb018c0d26beef29630d1c816a
CHRISTOFER EZEQUIEL DALPIAZ <christofer.dalpiaz@alunos.sc.senac.br>
2025-04-16 19:53:56 -0300

    Adicionando cliente

--- gerenciador-bancario/src/Main.java
+++ gerenciador-bancario/src/Main.java
@@ -1,15 +1,65 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner s = new Scanner(System.in);
+        //matriz clientes
+        ArrayList<ArrayList<Object>> clientes = new ArrayList<>();
+        int nConta;
+        boolean estaAtivo = true;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        while (estaAtivo) {
+            System.out.println("Digite o n° da opção que deseja:\n" +
+                    "1 - Cadastrar contas\n" +
+                    "2 - Realizar depósitos\n" +
+                    "3 - Realizar saques\n" +
+                    "4 - Consultar saldo\n" +
+                    "5 - Consultar histórico de movimentações\n" +
+                    "6 - Transferir entre contas\n" +
+                    "0 - Sair");
+            int opcao = Integer.parseInt(s.nextLine());
+
+            switch (opcao) {
+                case 1:
+                    System.out.println("Digite o nome do cliente:");
+                    String nome = s.nextLine();
+
+                    System.out.println("Digite o saldo inicial:");
+                    double saldo = Integer.parseInt(s.nextLine());
+
+                    ArrayList<Object> novoCliente = new ArrayList<>();
+                    novoCliente.add(nome);
+                    novoCliente.add(saldo);
+                    clientes.add(novoCliente);
+
+                    nConta = clientes.size() - 1;
+
+                    System.out.println("Cliente cadastrado com sucesso!");
+                    System.out.println("N° da conta: " + nConta);
+                    break;
+
+                case 2:
+
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                //Sair
+                case 0:
+                    System.out.println("Encerrando...");
+                    estaAtivo = false;
+                    break;
+
+                default:
+                    System.out.println("Opção inválida. Tente novamente");
+            }
         }
     }
 }
